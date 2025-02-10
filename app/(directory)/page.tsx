b5906cbd30a040f2514baf613609c44c85e734a7
Ridwan Lawal <lawal6382@gmail.com>
2025-02-10 14:46:17 +0100

    Complete the homepage

--- app/(directory)/page.tsx
+++ app/(directory)/page.tsx
@@ -1,16 +1,12 @@
-import PitchCard from "@/app/_components/pitches/PitchCard";
 import Header from "@/app/_ui/Header";
+import Pitches from "@/app/_components/pitches/Pitches";
 import React from "react";
 
 export default function Page() {
   return (
     <div className="pb-12">
       <Header />
-
-      {/* Pitches */}
-      <div>
-        <PitchCard />
-      </div>
+      <Pitches />
     </div>
   );
 }
